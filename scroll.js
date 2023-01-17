7caf92617e47141c616404342d817d86df2711ae
Corey <coreydickerson042@gmail.com>
2023-01-17 13:34:08 -0500

    general updates

--- scroll.js
+++
@@ -1,9 +0,0 @@
-function scrolll() {
-    var left = document.querySelector(".scroll-img")
-    left.scrollBy(350, 0)
-}
-
-function scrollr() {
-    var left = document.querySelector(".scroll-img")
-    left.scrollBy(-350, 0)
-}
